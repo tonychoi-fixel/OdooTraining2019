ce3583f5151f6c4ae095d68699c0bc79ba864484
tonychoi-fixel <54478101+tonychoi-fixel@users.noreply.github.com>
2019-10-21 16:15:55 -0700

    Update customer.py

--- technical-training-13.0-01-models/library/models/customer.py
+++ technical-training-13.0-01-models/library/models/customer.py
@@ -4,4 +4,4 @@ from odoo import api, models, fields, _
 class Customer(models.Model):
     _name = "library.customer"
     name = fields.Text(required=True, string="Customer Name")
-    book_id = fields.Many2One("library.book")
+    book_id = fields.Many2one("library.book")
